349690a7e39fa10af049e880f769a82a596557fb
stephenhawking832 <rbudnick22@gmail.com>
2025-08-17 23:08:28 +0300

    add cors

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://hapitron.online", 
+    "https://vote.hapitron.online", 
 ]
 
 app.add_middleware(
