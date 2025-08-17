abed140f3b97c65038c9f0b36d33613009790cc9
stephenhawking832 <rbudnick22@gmail.com>
2025-08-17 23:00:33 +0300

    add cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.api.v1.api import api_router
 from app.core.config import settings
@@ -9,6 +9,21 @@ app = FastAPI(
     openapi_url="/api/v1/openapi.json"
 )
 
+
+# For development, this is your Vue dev server.
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://hapitron.online", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specific origins
+    allow_credentials=True, # Allows cookies to be included in requests
+    allow_methods=["*"],    # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],    # Allows all headers
+)
 # Include the main router with a global prefix
 app.include_router(api_router, prefix="/api/v1")
 
